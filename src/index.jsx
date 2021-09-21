3b947b566c56df7db7b6a9e2bfb1684f13a8aaef
ckruppe <c.kruppe@nfq.de>
2021-09-21 15:53:20 +0200

    fix(Lib): No silo export (#13)

--- src/index.jsx
+++ src/index.jsx
@@ -1,26 +1,10 @@
-import Col from './grid/Col';
-import Container from './grid/Container';
-import Row from './grid/Row';
-import Spacer from './grid/Spacer';
-import Hidden from './utils/Hidden';
-import {getConfig, getScreenClass, media} from './utils/lib';
-import ScreenBadge from './utils/ScreenBadge';
-import ScreenClassProvider, {ScreenClassContext} from './utils/ScreenClassProvider';
-import ScreenClassRender from './utils/ScreenClassRender';
-import Visible from './utils/Visible';
-
-export {
-    Col,
-    Container,
-    getConfig,
-    getScreenClass,
-    Hidden,
-    Row,
-    media,
-    ScreenBadge,
-    ScreenClassContext,
-    ScreenClassProvider,
-    ScreenClassRender,
-    Spacer,
-    Visible
-};
+export {default as Col} from './grid/Col';
+export {default as Container} from './grid/Container';
+export {default as Row} from './grid/Row';
+export {default as Spacer} from './grid/Spacer';
+export {default as Hidden} from './utils/Hidden';
+export {getConfig, getScreenClass, media} from './utils/lib';
+export {default as ScreenBadge} from './utils/ScreenBadge';
+export {default as ScreenClassProvider, ScreenClassContext} from './utils/ScreenClassProvider';
+export {default as ScreenClassRender} from './utils/ScreenClassRender';
+export {default as Visible} from './utils/Visible';
