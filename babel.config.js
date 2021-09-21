085e74bdc47824a35bb93901d845426615caa975
ckruppe <c.kruppe@nfq.de>
2021-09-21 17:02:12 +0200

    fix(Package): Add type module (#15)

--- babel.config.js
+++ babel.config.js
@@ -10,7 +10,7 @@ module.exports = {
         'add-module-exports'
     ],
     presets: [
-        ['@babel/preset-env'],
+        ['@babel/preset-env', {modules: false}],
         '@babel/preset-react'
     ]
 };
