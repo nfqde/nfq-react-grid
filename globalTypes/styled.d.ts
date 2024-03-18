80a44c48f4cd85c0dbc49a53d085b487f96ed430
Christoph Kruppe <c.kruppe@nfq.de>
2024-03-18 14:29:16 +0100

    fix(Types): More literal types for color functions (#107)

--- globalTypes/styled.d.ts
+++ globalTypes/styled.d.ts
@@ -2,9 +2,13 @@
 import 'styled-components';
 import type {Config} from '../src/defaultConfig';
 
+enum Colors {
+    testColor = '#000000'
+}
 // and extend them!
 declare module 'styled-components' {
     export interface DefaultTheme {
+        colors: Colors;
         nfqgrid: Config;
     }
 }
